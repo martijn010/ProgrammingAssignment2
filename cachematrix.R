382ccb4a368c41c8927b7624b8d8153ad7194b29
Martijn <martijnoeij@hotmail.com>
2017-07-18 16:20:20 +0200

    Improved Assignment week 3
    Some improvements made

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,7 @@
-## Caching the inverse of a matrix.
+## Peer Graded Assignment
+## R Programming, Week 3
 
-## This function creates a special "matrix" object that can cache its inverse.
+## makeCacheMatrix creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
         inv <- NULL
@@ -16,7 +17,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## This function computes the inverse of the special "matrix" returned by 
+## cacheSolve computes the inverse of the special "matrix" returned by 
 ## makeCacheMatrix above. If the inverse has already been calculated (and the 
 ## matrix has not changed), then cacheSolve should retrieve the inverse from the 
 ## cache.
